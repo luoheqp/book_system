714cd2b6b83851eb437934cfc18e8d08bbfcabbd
洛河桥盼 <leoljustsave@gmail.com>
2019-10-27 12:12:45 +0800

    update : global font style

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,9 @@ export default class App extends Vue {
 @import "./assets/styles/index.less";
 
 #app {
-  font-family: "SFNSRounded";
+  font-family: "SFNSRounded", -apple-system, system-ui, BlinkMacSystemFont,
+    Helvetica Neue, PingFang SC, Hiragino Sans GB, Microsoft YaHei, Arial,
+    sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   min-height: 100%;
