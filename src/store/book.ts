b8ed0151d215183f3f69eb848b2e80e6e6d76810
洛河桥盼 <leoljustsave@gmail.com>
2019-10-29 16:55:57 +0800

    add vuex init

---
+++ src/store/book.ts
@@ -0,0 +1,10 @@
+import Mock from "mockjs";
+let bookinfo = Mock.mock({});
+
+let book = {
+  state: {
+    bookname: "this is bookname"
+  }
+};
+
+export default book;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,18 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+// modules
+import user from "./user";
+import book from "./book";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    token: "this is a token"
+  },
+  modules: {
+    user,
+    book
+  }
 });
