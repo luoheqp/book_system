2dc433cb5126445adbf757aa9ef7150832faaa74
洛河桥盼 <leoljustsave@gmail.com>
2019-12-12 20:23:50 +0800

    update / clean useless code in write page

--- src/views/Write/index.vue
+++ src/views/Write/index.vue
@@ -2,10 +2,6 @@
   <div class="write-wrap">
     <div class="write">
       <h3>write page</h3>
-      <!-- <Editor></Editor> -->
-      <input type="text" v-model="txt" />
-      <input type="file" @change="handleChange" ref="file" />
-      <input type="button" value="submit" @click="handleSubmit" />
     </div>
   </div>
 </template>
@@ -14,33 +10,10 @@
 import { Vue, Component } from "vue-property-decorator";
 import axios from "axios";
 
-// import "tui-editor/dist/tui-editor.css";
-// import "tui-editor/dist/tui-editor-contents.css";
-// import "codemirror/lib/codemirror.css";
-// import { Editor } from "@toast-ui/vue-editor";
-
-@Component({
-  // components: { Editor }
-})
+@Component({})
 export default class Write extends Vue {
   public content: any = new FormData();
   public txt: any = "this is a txt";
-
-  handleChange(e: any) {
-    console.log(e.target.files[0]);
-    this.content.append("filename", e.target.files[0]);
-    console.log(this.content.filename);
-  }
-
-  handleSubmit() {
-    this.content.append("textinfo", this.txt);
-    axios({
-      method: "POST",
-      url: "/book/postBookInfo",
-      data: this.content,
-      headers: {}
-    });
-  }
 
   mounted() {}
 }
